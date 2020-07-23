dea7afab1b14d41691bebd078ce8147c59284a0b
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-23 11:53:35 +0800

    [FizzBuzz][jayden] test:should_return_fizzbuzz_when_count_off_given_15

--- src/test/java/junit5/tdd/FizzBuzzTest.java
+++ src/test/java/junit5/tdd/FizzBuzzTest.java
@@ -43,4 +43,16 @@ public class FizzBuzzTest {
         //then
         assertEquals("Buzz", res);
     }
+
+    @Test
+    void should_return_fizzbuzz_when_count_off_given_15() {
+        //given
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        //when
+        String res = fizzBuzz.play(15);
+
+        //then
+        assertEquals("FizzBuzz", res);
+    }
 }
