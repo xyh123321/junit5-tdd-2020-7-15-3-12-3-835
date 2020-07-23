89069f8f03da289714cb661718166ff324d5adf9
JAYDEN Y H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEJA3@OOCL.COM>
2020-07-23 11:55:03 +0800

    [FizzBuzz][jayden] feature: pass should_return_fizzbuzz_when_count_off_given_15

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,11 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String play(int inputNum) {
+
+        if(inputNum %3 == 0 && inputNum %5 == 0){
+            return "FizzBuzz";
+        }
+
         if(inputNum %3 == 0) {
             return "Fizz";
         }
